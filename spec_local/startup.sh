fe3785f14d87c87fde4e444066ccf63f8bb5d44a
KRP <kunal.r.rajpurohit@gmail.com>
2021-03-03 22:43:50 -0800

    add linkerd

--- spec_local/startup.sh
+++ spec_local/startup.sh
@@ -1,2 +1,5 @@
-bazel run //spec_local:k8s.apply
-bazel run //spec_local:deployment_gateway.apply --platforms=@build_bazel_rules_nodejs//toolchains/node:linux_amd64
+kubectl config view >> spec_local/config;
+bazel run //spec_local:controllers.apply;
+linkerd check;
+bazel run //spec_local:k8s.apply;
+bazel run //spec_local:deployment_gateway.apply --platforms=@build_bazel_rules_nodejs//toolchains/node:linux_amd64;
